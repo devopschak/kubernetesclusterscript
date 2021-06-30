135789612b232401547a71294853d3aee549478e
chandracherukuri <42809378+chandracherukuri@users.noreply.github.com>
2021-06-30 08:10:40 +0530

    Add files via upload

---
+++ kc.sh
@@ -0,0 +1,42 @@
+#!/bin/bash/
+sudo -i
+sudo apt-get update -y
+sudo apt-get install -y apt-transport-https
+
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+
+cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+
+apt-get update -y
+
+swapoff -a
+sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
+
+modprobe br_netfilter
+sysctl -p
+sudo  sysctl net.bridge.bridge-nf-call-iptables=1
+
+apt install docker.io -y
+
+usermod -aG docker ubuntu
+
+systemctl restart docker
+systemctl enable docker.service
+
+apt-get install -y kubelet kubeadm kubectl kubernetes-cni
+systemctl daemon-reload
+systemctl start kubelet
+systemctl enable kubelet.service
+
+kubeadm init 
+
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
+
+kubectl get nodes
+kubectl get pods --all-namespaces

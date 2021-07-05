c8af1b8bda70b0f7105afca903a4bccdfb82231e
devopschak <devopschak@gmail.com>
2021-07-05 00:56:22 -0400

    Update kc.sh

--- kc.sh
+++ kc.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
-sudo apt-get update
-sudo apt-get install apt-transport-https
+#!/bin/bash/
+sudo apt-get update -y
+sudo apt-get install -y apt-transport-https
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
-deb https://apt.kubernetes.io/ kubernetes-xenial mainhttps://github.com/devopschak/kubernetesclusterscript
+deb https://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 apt-get update -y
 swapoff -a
